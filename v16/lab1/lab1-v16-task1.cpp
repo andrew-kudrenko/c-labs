9a15c9807aede75d4ccab4dd36f5a5853f6484bb
Andrew <48319785+andrew-kudrenko@users.noreply.github.com>
2021-02-19 03:29:31 +0300

    Create lab1-v16-task1.cpp

---
+++ v16/lab1/lab1-v16-task1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int foo, bar;
+    
+    std::cout << "The work has been done by Andrew Kudrenko, IT-202" << std::endl;
+    
+    std::cout << "Enter two any integers" << std::endl; 
+    std::cin >> foo >> bar;
+    std::cout << "Sum of the numbers equals " <<  foo + bar;
+    
+    return 0;
+}
