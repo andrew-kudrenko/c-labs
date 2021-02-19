516423de3e49ec10067f2d07010d60964b7d93d0
Andrew <48319785+andrew-kudrenko@users.noreply.github.com>
2021-02-19 03:28:48 +0300

    Create lab1-v16-task1.c

---
+++ v16/lab1/lab1-v16-task1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int foo, bar;
+    
+    printf("The work has been done by Andrew Kudrenko, IT-202\n");
+    
+    printf("Enter two any integers\n"); 
+    scanf("%d %d", &foo, &bar);
+    printf("Sum of the numbers equals %d", foo + bar);
+    
+    return 0;
+}
