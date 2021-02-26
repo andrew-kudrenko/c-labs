6e3a8d4067cf013bc1f1386e94e4bd13d1b1e0b2
Andrew <48319785+andrew-kudrenko@users.noreply.github.com>
2021-02-26 10:15:59 +0300

    Update lab1-v16-task2-while.cpp

--- v16/lab1/lab1-v16-task2-while.cpp
+++ v16/lab1/lab1-v16-task2-while.cpp
@@ -18,20 +18,39 @@ int main()
   
   std::cout << std::endl << "Function values will be below\n" << std::endl;
   
+  double avg = 0, max;
   int i = 0;
   
-  while (i < n)
+  while(i < n)
   {
-    double x = a + i * h,
-      p = sqrtf((1 / x) * (powf(x, 2) - 1)), 
-      q = powf(cosf(abs(x) / 3.), 2),
-      r = log10(1 / (x + 1)),
-      y = p * q + r; 
-      
-    std::cout << "x = " << x << "\t\ty = " << y << std::endl;
+    double x = a + i * h;
     
-    ++i;
-  }
+    if ((x != 0) && (x != 1))
+    {    
+        double p = sqrtf((1 / x) * (powf(x, 2) - 1)), 
+          q = powf(cosf(abs(x) / 3.), 2),
+          r = log10(1 / (x + 1)),
+          y = p * q + r;
+        
+        if ((i == 0) || (y > max))
+        {
+            max = y;
+        }
+        
+        avg += y;
+        std::cout << "x = " << x << "\t\ty = " << y << std::endl;
+    } else {
+        std::cout << "Incorrect value. Zero division is not allowed!" << std::endl; 
+    }
+    
+    ++i;   
+    }
+      
+      avg /= n;
+      
+      std::cout << std::endl;
+      std::cout << "Average equals " << avg << std::endl;
+      std::cout << "Max value is " << max << std::endl;
    
   return 0;
 }
